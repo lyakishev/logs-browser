53997dc950d8fe8252614bb7b60359dcd65e34c8
Lyakishev Andrey <lyakishev_a_v@mail.ru>
2010-12-11 17:10:30 +0300

    ?????? ???? ?????? ????? ?????.

--- parse.py
+++ parse.py
@@ -1,17 +1,45 @@
 from pyparsing import *
+from datetime import datetime, timedelta
+import pdb
+
+
+
+def parse_date(d):
+    try:
+        dformat = datetime.strptime(d[0], "%Y%m%d")
+    except ValueError:
+        try:
+            dformat = datetime.strptime(d[0], "%y%m%d")
+        except:
+            dformat = datetime.strptime(d[0], "%d%m%Y")
+    return dformat
+
+def hour_to_date(d):
+    #pdb.set_trace()
+    try:
+        return d['date']+timedelta(int(d['hour'])/24.)
+    except KeyError:
+        return d['date']
+        
 
 #sep = Literal(".") | Literal("_")
 sep = Suppress(Literal("_") | Literal("-") | Literal("."))
-date = Combine((Word(nums, exact=4) | Word(nums, exact=2))  + Optional(sep) +\
+date_with_full_year = (Word(nums, exact=4)  + Optional(sep) +\
     Word(nums, exact=2) + Optional(sep) + Word(nums, exact=2))
+date_with_short_year = (Word(nums, exact=2)  + Optional(sep) +\
+    Word(nums, exact=2) + Optional(sep) + Word(nums, exact=2))
+date = Combine( date_with_full_year | date_with_short_year).setParseAction(parse_date)('date')
 time = Combine(Word(nums, exact=2)  + Optional(sep) +\
     Word(nums, exact=2) + Optional(sep) + Word(nums, exact=2))
-hour = Word(nums, max=2)
+mgbeg = Word(nums,exact=13)
+hour = Word(nums, max=2)('hour')
 counter = Literal(".")+Word(nums, max=2)("counter")
-datetime = date + Optional(sep + hour | time+counter) # (hour | time))
+date_time = (date + Optional(sep + hour | time+counter)).setParseAction(hour_to_date)("datetime") # (hour | time))
 ext = Literal(".")+Word(alphas, exact=3)("ext")+Optional(counter | sep+date)+LineEnd()
-logname = SkipTo(date | sep+date | ext) #delimitedList(Word(alphanums),'.') | delimitedList(Word(alphanums), '_')
-filename=Optional(datetime+sep)+logname("logname")+Optional(Optional(sep)+datetime)+Optional(logname)+ext#+ext
+logname = SkipTo( Optional(sep)+date | sep+date_time | ext) #delimitedList(Word(alphanums),'.') | delimitedList(Word(alphanums), '_')
+filename=Optional(mgbeg+sep)+Optional(date_time+sep)+logname("logname")+Optional(Optional(sep)+date_time)+Optional(logname)("logname2")+ext#+ext
+#filename=logname("logname")+Literal("-")+Word(nums)+ext#+ext
+#filename.setParseAction(lambda t: ''.join([t['logname'],t['logname2']]))
 #filename = datetime+sep+logname+Optional(sep)+Optional(datetime)+sep+ext+Optional(sep+date+sep+counter)
 
 print filename.parseString("ProcessorService.txt.2010-12-09")
@@ -35,7 +63,13 @@ print filename.parseString("CounterFormatter20101209202233.27.txt")
 #print filename.parseString("RatingEngine_2010-12-10-07-51-36.0563798.txt") #!
 print filename.parseString("psp_20101210_11.log")
 print filename.parseString("psp_timer_20101210_12.log")
-print filename.parseString("4_3_0.000005.log")
-#print filename.parseString("foris_catalogue_admin-101208.log") #!
-#print filename.parseString("0122010000001-EMAILCON_EMAILCON_01_error.log") #nag-tc-05 MG
+#print filename.parseString("4_3_0.000005.log")
+print filename.parseString("foris_catalogue_admin-101208.log") #! this is date??
+print filename.parseString("0122010000001-EMAILCON_EMAILCON_01_error.log") #nag-tc-05 MG
 #nag-tc-06
+#for f in os.listdir("/home/user"):
+#    try:
+#        print os.path.getmtime(os.path.join("/home/user",f))
+#    except:
+#        pass
+
