c8f71d98f9a8ec8c5097cbe0bde3a040eaa3c4ce
Lyakishev Andrey <Lyakishev Andrey@localhost>
2011-02-10 09:59:39 +0300

    ????????? plsql analyzer.

---
+++ src/db_procs.py
@@ -0,0 +1,74 @@
+import cx_Oracle
+
+class ProcAnalyzer():
+    def __init__(self, proc_string, db):
+        self.proc = proc_string
+        self.db = db
+        self.split()
+        self.connect()
+        self.check()
+        self.proc_body()
+
+    def connect(self):
+        self.conn = cx_Oracle.connect(self.db)
+        self.cur = self.conn.cursor()
+
+    def split(self):
+        self.package, self.proc_name = self.proc.split('.')
+    
+    def check(self):
+        self.cur.execute("select count(*) from user_objects where object_name\
+            like :pack", pack=self.package)
+        exist = self.cur.fetchall()[0][0]
+        if not exist:
+            print "Package does not exist"
+            return False
+        else:
+            return True
+
+    def proc_body(self):
+        self.cur.execute("select text from user_source where type='PACKAGE "\
+            "BODY' and name=:pack order by line", pack=self.package)
+        source = self.cur.fetchall()
+        proc_source = []
+        line_number = 0
+        for n, l in enumerate(source):
+            line = l[0]
+            if line.startswith("--"):
+                continue
+            lower_line = line.lower()
+            if self.proc_name.lower() in lower_line:
+                proc_source.append(line)
+                if "procedure" in lower_line:
+                    self.type_ = "proc"
+                elif "function" in lower_line:
+                    self.type_ = "func"
+                line_number = n+1
+                break
+        begin_end = 0
+        for l in source[line_number:]:
+            line = l[0]
+            if "END" in line:
+                begin_end-=1
+                if begin_end == 0:
+                    proc_source.append(line)
+                    break
+            if "BEGIN" in line:
+                begin_end+=1
+            proc_source.append(line)
+        self.proc_source = "".join(proc_source)
+
+    def call(self):
+        if selt.type_ == "proc":
+            pass
+            #cursor.callproc
+        elif self.type_ == "func":
+            pass
+            #cursor.callfunc
+
+#oracle ca
+
+if __name__ == "__main__":
+    pa = ProcAnalyzer("CUSTOMER_GET.GET_PERSONAL_ACCOUNT_BY_ID",
+                     "tf2_cust/cust@heine")
+    print pa.type_
