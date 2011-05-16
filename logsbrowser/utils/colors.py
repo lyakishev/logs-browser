3e7797ebeca8a8fb836470819dc922a5f781a091
Lyakishev Andrey <Lyakishev Andrey@localhost>
2011-05-16 09:05:49 +0400

    ?????????? colors.py

--- logsbrowser/utils/colors.py
+++ logsbrowser/utils/colors.py
@@ -1,4 +1,12 @@
-def check_color(color):
-    return True
+import gtk
 
 class ColorError(Exception): pass
+
+def check_color(color_value):
+    try:
+        gtk.gdk.color_parse(color_value)
+    except ValueError:
+        return False
+    else:
+        return True
+
