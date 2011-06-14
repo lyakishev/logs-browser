1136a910dc4709e2b1fba69441abd0f4071bef5f
Lyakishev Andrey <Lyakishev Andrey@localhost>
2011-06-14 16:25:50 +0400

    while ?????? for ? seek_block_read, ?.?. (x)range ?? ???????? ? ????? ???????? ???????.

--- logsbrowser/lparser/files/readers.py
+++ logsbrowser/lparser/files/readers.py
@@ -25,18 +25,22 @@ def mmap_block_read(file_, block_size=8192):
             yield line
 
 def seek_block_read(file_, block_size=8192):
-    mf_size = end_pos = size
-    for i in xrange(mf_size-1-block_size, -1, -block_size):
-        file_.seek(i, 0)
-        text_block = file_.read(end_pos+1-i)
-        ret_pos = text_block.find("\n")
-        if ret_pos >= 0:
-            for line in text_block.splitlines(True)[-1:0:-1]:
-                yield line
-        else:
-            ret_pos = 0
-        end_pos = i + ret_pos
-    file_.seek(0, 0)
-    text_block = file_.read(end_pos+1)
-    for line in text_block.splitlines(True)[::-1]:
-        yield line
+    file_.seek(0,2)
+    mf_size = end_pos = file_.tell()
+    if mf_size > 0:
+        i = mf_size-1-block_size
+        while i > 0:
+            file_.seek(i, 0)
+            text_block = file_.read(end_pos+1-i)
+            ret_pos = text_block.find("\n")
+            if ret_pos >= 0:
+                for line in text_block.splitlines(True)[-1:0:-1]:
+                    yield line
+            else:
+                ret_pos = 0
+            end_pos = i + ret_pos
+            i -= block_size
+        file_.seek(0, 0)
+        text_block = file_.read(end_pos+1)
+        for line in text_block.splitlines(True)[::-1]:
+            yield line
