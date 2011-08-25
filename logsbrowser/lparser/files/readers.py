67ddc16d3fbe339c39bfe3def0914fdb96890af3
Lyakishev Andrey <Lyakishev Andrey@localhost>
2011-08-25 10:32:24 +0400

    ????????? ??????? ?? mmap, ? ?? ?????????? ?? ??????? ? ???????????.

--- logsbrowser/lparser/files/readers.py
+++ logsbrowser/lparser/files/readers.py
@@ -25,6 +25,35 @@ def mmap_block_read(file_, block_size=8192):
         for line in seek_block_read(file_, block_size):
             yield line
 
+def mmap_block_read2(file_, block_size=8192):
+    try:
+        with closing(mmap.mmap(file_.fileno(), 0,
+                     access=mmap.ACCESS_READ)) as data:
+            mf_size = end_pos = end_msg_pos = start_msg_pos = len(data)
+            for i in xrange(mf_size-1-block_size, -1, -block_size):
+                lines = data[i:end_pos+1].splitlines(True)
+                try:
+                    lines[1]
+                except IndexError:
+                    ret_pos = 0
+                else:
+                    for line in lines[-1:0:-1]:
+                        get_msg = (yield line)
+                        start_msg_pos -= len(line)
+                        if get_msg:
+                            yield data[start_msg_pos:end_msg_pos]
+                            end_msg_pos = start_msg_pos
+                    ret_pos = len(lines[0]) - 1
+                end_pos = i + ret_pos
+            for line in data[0:end_pos+1].splitlines(True)[::-1]:
+                get_msg = (yield line)
+                start_msg_pos -= len(line)
+                if get_msg:
+                    yield data[start_msg_pos:end_msg_pos]
+                    end_msg_pos = start_msg_pos
+    except (WindowsError, MemoryError, ValueError):
+        pass
+
 def seek_block_read(file_, block_size=8192):
     file_.seek(0,2)
     mf_size = end_pos = file_.tell()
--- logsbrowser/lparser/files/worker.py
+++ logsbrowser/lparser/files/worker.py
@@ -2,7 +2,7 @@
 
 import os
 import time
-from readers import mmap_block_read, seek_block_read
+from readers import mmap_block_read, seek_block_read, mmap_block_read2
 from operator import itemgetter
 from utils.common import to_unicode, isoformat
 import config
@@ -16,7 +16,7 @@ def get_start_date(file_, pformat, cdate, pfunc):
             return start_date
 
 
-def filelogworker(dates, path, log, funcs):
+def filelogworker1(dates, path, log, funcs):
     pformat, pfunc, need_date = funcs
     if need_date:
         try:
@@ -54,6 +54,47 @@ def filelogworker(dates, path, log, funcs):
                                0,
                                to_unicode("".join(msg))), clines
                     msg.clear()
+                    clines = 0
+    except IOError:
+        raise StopIteration
+
+def filelogworker(dates, path, log, funcs):
+    pformat, pfunc, need_date = funcs
+    if need_date:
+        try:
+            cdate = time.localtime(os.path.getctime(path))
+        except WindowsError:
+            print "WindowsError: %s" % path
+            raise StopIteration
+    else:
+        cdate = None
+    try:
+        with open(path, 'rU') as file_:
+            start_date = get_start_date(file_, pformat, cdate, pfunc)
+            if start_date > dates[1]:
+                raise StopIteration
+            comp = [p for p in path.split(os.sep) if p][0]
+            path = to_unicode(path)
+            clines = 0
+            reader = mmap_block_read2(file_, 16*1024)
+            for string in reader:
+                parsed_date = pfunc(string, cdate, pformat)
+                if not parsed_date:
+                    clines += 1
+                else:
+                    if parsed_date < dates[0]:
+                        raise StopIteration
+                    if parsed_date <= dates[1]:
+                        msg = reader.send(1)
+                        clines += 1
+                        yield (parsed_date,
+                               comp,
+                               log,
+                               ("ERROR" if ("Exception" in msg and "  at " in msg)
+                                       else "?"),
+                               path,
+                               0,
+                               to_unicode(msg)), clines
                     clines = 0
     except IOError:
         raise StopIteration
